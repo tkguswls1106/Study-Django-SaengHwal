38cf6c97597af0e00ed322886812974d2b33f459
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-03-24 18:16:09 +0900

    Update myapp_views.py

--- myapp_views.py
+++ myapp_views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render, HttpResponse
 import random
 
-# Create your views here.
+# topics라는 리스트를 만들어 만들어둔 딕셔너리들을 그룹핑해준다.
+topics = [
+    {'id':1, 'title':'routing', 'body':'Routing is ..'},
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'Model', 'body':'Model is ..'},
+]
 
 # views에다가 클라이언트로 정보를 전송하기위한 함수를 작성한다. 먼저 index 함수를 작성한다.
 def index(request):  # 이 함수는 첫번째 파라미터의 인자로 요청과 관련된 여러가지 정보가 들어오도록 약속되어있는 객체를 전달해주도록 되어있다.
                      # => request 라는 이름의 파라미터를 적어준다. 사실 뭘적든 상관은 없지만 관습적으로 request라고 적어주는것을 추천한다.
-    return HttpResponse('<h1>Random</h1>'+str(random.random()))  # HttpResponse()는 http를 이용해서 응답을 하겠다는 의미의 객체이다.
-                                                                 # 그리고 그 인자로 전송하고싶은 값을 그 안에 적는다.
-                                                                 # ('<h1>Random</h1>'+random.random()) 으로 적으면
-                                                                 # +의 앞에는 문자열인데 뒤는 숫자이므로 에러가나서 str(random.random())로 str을 붙여준다.
+    global topics  # topics라는 변수를 이 함수에서 사용하기위해 전역변수로 지정함.
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'  # f는 파이썬의 f-string 이라는 문자열 포매팅 방법인데, 도장깨기 파이썬md파일에 정리해두겠다.
+    return HttpResponse(f'''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            {ol}
+        </ol>
+        <h2>Welcome</h2>
+        Hello, Django
+    </body>
+    </html>
+    ''')
 
 def create(request):
-    return HttpResponse('Create')
+    return HttpResponse('Create')  # HttpResponse()는 http를 이용해서 응답을 하겠다는 의미의 객체이다.
+                                   # 그리고 그 인자로 전송하고싶은 값을 그 안에 적는다.
 
 def read(request, id):
     return HttpResponse('Read!'+id)
