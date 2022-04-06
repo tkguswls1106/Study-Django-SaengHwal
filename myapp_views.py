6a38366a8bce4a802f368569e0c88bbe15f8a00d
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-04-06 22:33:23 +0900

    Update myapp_views.py

--- myapp_views.py
+++ myapp_views.py
@@ -9,9 +9,19 @@ topics = [
     {'id':3, 'title':'Model', 'body':'Model is ..'},
 ]
 
-def HTMLTemplate(articleTag):
+def HTMLTemplate(articleTag, id=None):  # id가 입력되지 않았을때를 대비하기위해 id=None 으로 지정해둔것이다.
     global topics  # topics라는 변수를 이 함수에서 사용하기위해 전역변수로 지정함.
                    # 더 자세한건 도장깨기 파이썬md파일에 정리해두겠다.
+    contextUI = ''  # id가 있을때에만 delete UI버튼을 나오게할것임 => 즉, 홈버튼에서는 delete UI버튼이 안나오도록 만들어볼것임.
+    if id != None:
+        contextUI = f'''
+                    <li>
+                <form action="/delete/" method="post">
+                    <input type="hidden" name="id" value={id}>
+                    <input type="submit" value="delete">
+                </form>
+            </li>
+        '''
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'  # f는 파이썬의 f-string 이라는 문자열 포매팅 방법인데, 도장깨기 파이썬md파일에 정리해두겠다.
@@ -25,6 +35,7 @@ def HTMLTemplate(articleTag):
         {articleTag}
         <ul>
             <li><a href="/create/">create</a></li>
+            {contextUI}
         </ul>
     </body>
     </html>
@@ -46,7 +57,7 @@ def read(request, id):
     for topic in topics:
         if topic['id'] == int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
-    return HttpResponse(HTMLTemplate(article))
+    return HttpResponse(HTMLTemplate(article, id))
 
 @csrf_exempt
 def create(request):  # 현재 상태는 그냥 홈페이지 접속해서 create 링크만 누르면 요청 메서드가 'GET'이고,
@@ -67,7 +78,22 @@ def create(request):  # 현재 상태는 그냥 홈페이지 접속해서 create
         body = request.POST['body']
         newTopic = {"id":nextId, "title":title, "body":body}
         topics.append(newTopic)  # 딕셔너리는 append 사용이 안되지만, topics는 딕셔너리를 그룹핑한 리스트이므로 사용이 가능하다.
+                                 # topics에 newTopic 추가해서 사이트의 글 목록에 더 create 함.
         url = '/read/'+str(nextId)
         nextId = nextId + 1
         return redirect(url)  # redirect() 안의 매개변수의 url로 이동해라 라는 뜻이다.
                               # redirect 리디렉트와 관련된 것은 django_생활코딩 md 파일에 정리해두겠다.
+
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method == 'POST':
+        id = request.POST['id']  # id는 입력받은 id값이다. (아마도?)
+        newTopics = []  # newTopic과 헷갈리지말자!
+        for topic in topics:  # for문으로 순회를 하면서 id와 일치하지않는 애들을 newTopics 리스트에 추가하여 넣겠다.
+            if topic['id'] != int(id):  # 삭제안할것들을 걸러냄.
+                newTopics.append(topic)
+        topics = newTopics  # 삭제할 입력된 id가 다른것들(삭제안할것들)을 newTopics 리스트에 넣어두고 topics 와 일치시키면
+                            # 그 topics는 결국 '삭제되고 남은것들의 집합체'가 된다. => 홈으로 결국 이동시킬것
+                            # 참고로 삭제안할거 제외하고 나머지를 전부 topic을 다시 만든것이므로, 이는 결국 하나를 삭제시킨것과 동일한 효과라는 것이다.
+        return redirect('/')  # 이 작업이 끝나면 사용자를 홈으로 보내버린다.
