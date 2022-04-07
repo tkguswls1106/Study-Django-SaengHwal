b6aeb6f9df60b0fa4e0671939a34c518b873d55e
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-04-07 19:42:02 +0900

    Update myapp_urls.py

--- myapp_urls.py
+++ myapp_urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('read/<id>/', views.read),  # <id>는 꺽새<>를 이용해서 바뀔수있는 값이 올수도 있다는 의미로 적은것이다.
                                     # myapp_views.py의 read함수에도 id를 추가해주자.
                                     # 이로써 예를들어 path를 /read/shj/ 라고 입력하면 Read!shj 라고 출력된다.
+    path('update/<id>/', views.update),
     path('delete/', views.delete)
 ]
 # 이렇게 각각의 경로로 접속을 했을때 views로 전달을 한다.
